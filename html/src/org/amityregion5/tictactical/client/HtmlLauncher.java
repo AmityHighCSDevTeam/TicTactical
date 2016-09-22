1fa2e34b79e549959ee39574857eff0ca3c2f555
SergeySave <sergeysav.nn@gmail.com>
2016-09-22 13:47:46 -0400

    Initial Commit

---
+++ html/src/org/amityregion5/tictactical/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package org.amityregion5.tictactical.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import org.amityregion5.tictactical.TicTactical;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new TicTactical();
+        }
+}
