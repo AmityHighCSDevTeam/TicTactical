6da7d8e38a02c617ba7df71076eb44db89c868cd
Timothy Frieden <friedenti19@amityregion5.org>
2016-09-28 15:16:16 -0400

    starts project

--- core/src/org/amityregion5/tictactical/TicTactical.java
+++ core/src/org/amityregion5/tictactical/TicTactical.java
@@ -1,33 +1,40 @@
 package org.amityregion5.tictactical;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class TicTactical extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class TicTactical extends Game {
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	}
+	
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		super.resize(width, height);
+	}
+	
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		super.resume();
+	}
+	
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		super.pause();
 	}
 }
