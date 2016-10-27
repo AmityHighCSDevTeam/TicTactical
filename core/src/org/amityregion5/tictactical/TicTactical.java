4b459f26f61ded2a27d0c6da7c25e3edf6671faa
Derpanieux <kevint2001@gmail.com>
2016-10-27 10:57:49 -0400

    Logic implements, click detection works, green to show selected grid

--- core/src/org/amityregion5/tictactical/TicTactical.java
+++ core/src/org/amityregion5/tictactical/TicTactical.java
@@ -6,215 +6,148 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class TicTactical extends Game {
-	
+	//is jus gam. why yew hef to be mad?
 	OrthographicCamera cam;
-	
+
 	Texture X;
 	Texture O;
 	Texture grid;
+	Texture selector;
 	SpriteBatch spritebatch;
 	private char[][] board = new char[9][9];
 	private boolean turn = false; // false is X, True is O
-	
-	int tX = 0;
-	int tY = 0;
-	int c = 0;
-	int mc = 60;
+	private int next_move = -1;
+	private char win = 0;
 	
 	@Override
 	public void create () {
 		cam = new OrthographicCamera();
-		
+
 		X = new Texture(Gdx.files.internal("X.png"));
 		O = new Texture(Gdx.files.internal("O.png"));
+		selector = new Texture(Gdx.files.internal("selector.png"));
 		grid = new Texture(Gdx.files.internal("grid.png"));
 		spritebatch = new SpriteBatch();
 		spritebatch.setBlendFunction(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
-		
-		board[0][0] = 'o';
-
-		/*
-		for(int i = 0; i < 9; i++){
-			for(int j = 0; j < 9; j++){
-				if(turn){
-					board[i][j] = 'x';
-				}else{
-					board[i][j] = 'o';
-				}
-				turn = !turn;
-			}
-		}
-		*/
+		 
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		int grid_size = Math.min(Gdx.graphics.getHeight(), Gdx.graphics.getWidth());
+		int miniboard_size = ((grid_size - 60) / 3) - 10;
+		int slot_size = (miniboard_size / 3) - (miniboard_size / 9);
 		
-		if (c < mc) {
-			c++;
-		} else {
-			c = 0;
-			board[tX][tY] = 0;
-			tY++;
-			tX += tY/9;
-			tY %= 9;
-			tX %= 9;
-			board[tX][tY] = 'o';
-		}
-		
-		if (Gdx.input.justTouched()) {
-			int in_x = Gdx.graphics.getHeight() - Gdx.input.getX();
-			int in_y = Gdx.graphics.getHeight() - Gdx.input.getY();
+		if (Gdx.input.justTouched() && Gdx.input.getX() <= grid_size + 30 && Gdx.input.getY() <= Gdx.graphics.getHeight() - 30 && Gdx.input.getY() >= 30 && Gdx.input.getX() >= 30) {
+			int in_x = Gdx.input.getX() - 30;
+			int in_y = Gdx.graphics.getHeight() - Gdx.input.getY() - 30;
 			int miniboard = -1;
 			int slot = -1;
-			if(30 < in_x && in_x < ((Gdx.graphics.getHeight() - 60) / 3) - 15){
-				//column 1
-				if(30 < in_y && in_y < ((Gdx.graphics.getHeight() - 60) / 3) - 15){
-					miniboard = 0; 
-				}else if(((Gdx.graphics.getHeight() - 60) / 3) - 15 < in_y && in_y < ((Gdx.graphics.getHeight() - 60) * 2 / 3) - 15){
-					miniboard = 3;
-				}else if(((Gdx.graphics.getHeight() - 60) * 2 / 3 < in_y && in_y < ((Gdx.graphics.getHeight() - 60)))){
-					miniboard = 6;
-				}	
-			}else if(((Gdx.graphics.getHeight() - 60) / 3) - 15 < in_x && in_x < ((Gdx.graphics.getHeight() - 60) * 2 / 3) - 15){
-				//column 2
-				if(30 < in_y && in_y < ((Gdx.graphics.getHeight() - 60) / 3) - 15){
-					miniboard = 1; 
-				}else if(((Gdx.graphics.getHeight() - 60) / 3) - 15 < in_y && in_y < ((Gdx.graphics.getHeight() - 60) * 2 / 3) - 15){
-					miniboard = 4;
-				}else if(((Gdx.graphics.getHeight() - 60) * 2 / 3 < in_y && in_y < ((Gdx.graphics.getHeight() - 60)))){
-					miniboard = 7;
+			
+			miniboard = (((in_x / (miniboard_size + 10))) % 3) + 3 * (in_y / (miniboard_size + 10));
+			slot = ((in_x - (miniboard % 3) * (miniboard_size + 10)) / ((miniboard_size + 10) / 3) % 3) + 3 * ((in_y - (miniboard / 3) * (miniboard_size + 10)) / ((miniboard_size + 10) / 3));
+			
+			if(!(miniboard == -1 || slot == -1) && board[miniboard][slot] == 0 && (miniboard == next_move || next_move == -1)){
+				
+				//Grids arranged as follows:
+				/*
+				 * 6 7 8
+				 * 3 4 5
+				 * 0 1 2
+				 * 
+				 */
+
+				//byte[] input = new byte[2];
+				if(turn){
+					board[miniboard][slot] = 'o';
+				}else{
+					board[miniboard][slot] = 'x';
 				}
-			}else if(((Gdx.graphics.getHeight() - 60) * 2 / 3 < in_x && in_x < ((Gdx.graphics.getHeight() - 60)))){
-				//column 3
-				if(30 < in_y && in_y < ((Gdx.graphics.getHeight() - 60) / 3) - 15){
-					miniboard = 2; 
-				}else if(((Gdx.graphics.getHeight() - 60) / 3) - 15 < in_y && in_y < ((Gdx.graphics.getHeight() - 60) * 2 / 3) - 15){
-					miniboard = 5;
-				}else if(((Gdx.graphics.getHeight() - 60) * 2 / 3 < in_y && in_y < ((Gdx.graphics.getHeight() - 60)))){
-					miniboard = 8;
+				turn = !turn;
+				char result = evaluate(board[miniboard], slot);
+				if(result == 'x'){    
+					for(int i = 0; i < 9; i++){
+						board[miniboard][i] = 'X';
+					}
+				}else if(result == 'o'){
+					for(int i = 0; i < 9; i++){
+						board[miniboard][i] = 'O';
+					}
+				}
+				char[] board0 = new char[9];
+				for(int i = 0; i < 9; i++){
+					board0[i] = board[i][0];
+				}
+				char winner = evaluate(board0, miniboard);
+				if(winner == 'X'){
+					win = 'X';
+				}else if(winner == 'O'){
+					win = 'O';;
+				}else{
+					int tie_test = 0;
+					for(int i = 0; i < 9; i++){
+						for(int j = 0; j < 9; j ++){
+							if(board[i][j] != ' '){
+								tie_test++;
+							}
+						}
+					}
+					if(tie_test == 81){
+						win = 'T';
+					}
+				}
+				if(board[slot][0] == 'X' || board[slot][0] == 'O'){
+					next_move = -1;
+				}else{
+					next_move = slot;
 				}
 			}
-			if(!(miniboard == -1 || slot == -1)){
-				
-			
-	        //Grids arranged as follows:
-	        /*
-	         * 6 7 8
-	         * 3 4 5
-	         * 0 1 2
-	         * 
-	         */
-	        
-	        //byte[] input = new byte[2];
-	        if(turn){
-	        	board[miniboard][slot] = 'o';
-	        }else{
-	        	board[miniboard][slot] = 'x';
-	        }
-	        turn = !turn;
-	        char result = evaluate(board[miniboard], slot);
-	        if(result == 'x'){    
-	           	for(int i = 0; i < 9; i++){
-	           		board[miniboard][i] = 'X';
-	            }
-	        }else if(result == 'o'){
-	            for(int i = 0; i < 9; i++){
-	                board[miniboard][i] = 'O';
-	            }
-	        }
-	        char[] board0 = new char[9];
-	        for(int i = 0; i < 9; i++){
-	            board0[i] = board[i][0];
-	        }
-	        char winner = evaluate(board0, miniboard);
-	        if(winner == 'X'){
-	            //The winner is X
-	        }else if(winner == 'O'){
-	            //The winner is O;
-	        }else{
-	            int tie_test = 0;
-	            for(int i = 0; i < 9; i++){
-	                for(int j = 0; j < 9; j ++){
-	                    if(board[i][j] != ' '){
-	                        tie_test++;
-	                    }
-	                }
-	            }
-	            if(tie_test == 81){
-	            	//tie game
-	            }
-	        }
-	            /*   
-	            valid_grids = validate_grids(board);
-	            if(valid_grids[slot]){
-	                for(int i = 0; i < 9; i++){
-	                    valid_grids[i] = false;
-	                }
-	                valid_grids[slot] = true;
-	                miniboard = slot;
-	            }else{
-	                miniboard = -1;
-	            }
-	            */
-	        }
-	    }
-	    
+		}
 
-		int grid_size = Math.min(Gdx.graphics.getHeight(), Gdx.graphics.getWidth());
-		int miniboard_size = ((grid_size - 60) / 3) - 15;
-		
+
 		spritebatch.setProjectionMatrix(cam.combined);
 		spritebatch.begin();{
 
 			spritebatch.draw(grid, 30, 30, grid_size - 60, grid_size - 60);
-			
-			//drawing column 1
-			if(board[0][0] == 'X'){
-				spritebatch.draw(X, 35, 35, ((grid_size - 60) / 3) - 15, ((grid_size - 60) / 3) - 15);
-			}else if(board[0][0] == 'O'){
-				spritebatch.draw(O, 35, 35, ((grid_size - 60) / 3) - 15, ((grid_size - 60) / 3) - 15);	
-			}else{
-				spritebatch.draw(grid, 35, 35, ((grid_size - 60) / 3) - 15, ((grid_size - 60) / 3) - 15);
+
+			for(int i = 1; i <= 3; i ++){
+				for(int j = 1; j <= 3; j ++){
+					spritebatch.draw(grid, 30 + (i * 2.5f) + ((grid_size - 60) * (i - 1)) / 3, 30 + (j * 2.5f) + ((grid_size - 60) * (j - 1)) / 3, miniboard_size, miniboard_size);
+					if((i - 1) + (j- 1) * 3 == next_move || next_move == -1){
+						spritebatch.draw(selector, 30 + (i * 2.5f) + ((grid_size - 60) * (i - 1)) / 3, 30 + (j * 2.5f) + ((grid_size - 60) * (j - 1)) / 3, miniboard_size, miniboard_size);
+					}
+				}	
 			}
-			
-				spritebatch.draw(grid, 35, 35 + (grid_size - 60) / 3, ((grid_size - 60) / 3) - 15, ((grid_size - 60) / 3) - 15);
-				spritebatch.draw(grid, 35, 35 + (grid_size - 60) * 2 / 3, ((grid_size - 60) / 3) - 15, ((grid_size - 60) / 3) - 15);
-			//drawing column 2
-			spritebatch.draw(grid, 40 + (grid_size - 60) / 3, 35, ((grid_size - 60) / 3) - 15, ((grid_size - 60) / 3) - 15);
-			spritebatch.draw(grid, 40 + (grid_size - 60) / 3, 35 + (grid_size - 60) / 3, ((grid_size - 60) / 3) - 15, ((grid_size - 60) / 3) - 15);
-			spritebatch.draw(grid, 40 + (grid_size - 60) / 3, 35 + (grid_size - 60) * 2 / 3, ((grid_size - 60) / 3) - 15, ((grid_size - 60) / 3) - 15);
-			
-			//drawing column 3
-			spritebatch.draw(grid, 40 + (grid_size - 60) * 2 / 3, 35, ((grid_size - 60) / 3) - 15, ((grid_size - 60) / 3) - 15);
-			spritebatch.draw(grid, 40 + (grid_size - 60) * 2 / 3, 35 + (grid_size - 60) / 3, ((grid_size - 60) / 3) - 15, ((grid_size - 60) / 3) - 15);
-			spritebatch.draw(grid, 40 + (grid_size - 60) * 2 / 3, 35 + (grid_size - 60) * 2 / 3, ((grid_size - 60) / 3) - 15, ((grid_size - 60) / 3) - 15);
-			
-			
+
 			//drawing x's and o's
 			for(int i = 0; i < 9; i++){
 				if(board[i][0] == 'X'){
-					spritebatch.draw(X, 40 + (grid_size - 60) * (i % 3) / 3, 35 + (grid_size - 60) * (i / 3) / 3, ((grid_size - 60) / 3) - 15, ((grid_size - 60) / 3) - 15);
+					spritebatch.draw(X, 30 + (((i % 3) + 1) * 2.5f) + ((grid_size - 60) * (i % 3)) / 3, 30 + ((i / 3) * 2.5f) + ((grid_size - 60) * (i / 3)) / 3, miniboard_size, miniboard_size);
 				}else if(board[i][0] == 'O'){
-					spritebatch.draw(O, 40 + (grid_size - 60) * (i % 3) / 3, 35 + (grid_size - 60) * (i / 3) / 3, ((grid_size - 60) / 3) - 15, ((grid_size - 60) / 3) - 15);
+					spritebatch.draw(O, 30 + (((i % 3) + 1) * 2.5f) + ((grid_size - 60) * (i % 3)) / 3, 30 + ((i / 3) * 2.5f) + ((grid_size - 60) * (i / 3)) / 3, miniboard_size, miniboard_size);
 				}else{
 					for(int j = 0; j < 9; j ++){
 						if(board[i][j] == 'x'){
-							spritebatch.draw(X, 40 + ((grid_size - 60) * (i % 3) / 3) + ((((grid_size - 60) / 3) - 15) / 3) * (j % 3), 35 + ((grid_size - 60) * (i / 3) / 3) + ((((grid_size - 60) / 3) - 15) / 3) * (j / 3), (((grid_size - 60) / 3) - 15) / 3, (((grid_size - 60) / 3) - 15) / 3);
+							spritebatch.draw(X, 32.5f + ((i % 3) * 2.5f)  + ((j % 3) * 2.5f) + ((grid_size - 60) * (i % 3)) / 3 + ((miniboard_size * (j % 3) / 3) + slot_size / 2) - (slot_size / 3), 32.5f + ((i / 3) * 2.5f)  + ((j / 3) * 2.5f) + ((grid_size - 60) * (i / 3)) / 3 + ((miniboard_size * (j / 3) / 3) + slot_size / 2) - (slot_size / 3), slot_size, slot_size);
 						}else if(board[i][j] == 'o'){
-							spritebatch.draw(O, 40 + ((grid_size - 60) * (i % 3) / 3) + ((((grid_size - 60) / 3) - 15) / 3) * (j % 3), 35 + ((grid_size - 60) * (i / 3) / 3) + ((((grid_size - 60) / 3) - 15) / 3) * (j / 3), (((grid_size - 60) / 3) - 15) / 3, (((grid_size - 60) / 3) - 15) / 3);
+							spritebatch.draw(O, 32.5f + ((i % 3) * 2.5f)  + ((j % 3) * 2.5f) + ((grid_size - 60) * (i % 3)) / 3 + ((miniboard_size * (j % 3) / 3) + slot_size / 2) - (slot_size / 3), 32.5f + ((i / 3) * 2.5f)  + ((j / 3) * 2.5f) + ((grid_size - 60) * (i / 3)) / 3 + ((miniboard_size * (j / 3) / 3) + slot_size / 2) - (slot_size / 3), slot_size, slot_size);
 						}else{
-							
+
 						}
 					}
 				}
 			}
 			
+			//printing win statements
+			{
+				//spritebatch.
+			}
 		}
 		spritebatch.end();
 	}
@@ -245,47 +178,47 @@ public class TicTactical extends Game {
 		// TODO Auto-generated method stub
 		super.pause();
 	}
-	
+
 	public static char evaluate(char[] b, int s){
-        if(b[s] != ' '){
-            if(s == 0){
-                if((b[0] == b[1] && b[1] == b[2]) || (b[0] == b[3] && b[3] == b[6]) || (b[0] == b[4] && b[4] == b[8])){
-                    return b[s];
-                }
-            }else if(s == 1){
-                if((b[0] == b[1] && b[1] == b[2]) || (b[1] == b[4] && b[4] == b[7])){
-                    return b[s];
-                }
-            }else if(s == 2){
-                if((b[0] == b[1] && b[1] == b[2]) || (b[2] == b[5] && b[5] == b[8]) || (b[2] == b[4] && b[4] == b[6])){
-                    return b[s];
-                }
-            }else if(s == 3){
-                if((b[0] == b[3] && b[3] == b[6]) || (b[3] == b[4] && b[4] == b[5])){
-                    return b[s];
-                }
-            }else if(s == 4){
-                if((b[3] == b[4] && b[4] == b[5]) || (b[1] == b[4] && b[4] == b[7]) || (b[0] == b[4] && b[4] == b[8] || (b[2] == b[4] && b[4] == b[6]))){
-                    return b[s];
-                }
-            }else if(s == 5){
-                if((b[2] == b[5] && b[5] == b[8]) || (b[3] == b[4] && b[4] == b[5])){
-                    return b[s];
-                }
-            }else if(s == 6){
-                if((b[6] == b[7] && b[7] == b[8]) || (b[0] == b[3] && b[3] == b[6]) || (b[2] == b[4] && b[4] == b[6])){
-                    return b[s];
-                }
-            }else if(s == 7){
-                if((b[6] == b[7] && b[7] == b[8]) || (b[1] == b[4] && b[4] == b[7])){
-                    return b[s];
-                }
-            }else if(s == 8){
-                if((b[6] == b[7] && b[7] == b[8]) || (b[2] == b[5] && b[5] == b[8]) || (b[0] == b[4] && b[4] == b[8])){
-                    return b[s];
-                }
-            }
-        }
-        return ' ';
-    }
+		if(b[s] != ' '){
+			if(s == 0){
+				if((b[0] == b[1] && b[1] == b[2]) || (b[0] == b[3] && b[3] == b[6]) || (b[0] == b[4] && b[4] == b[8])){
+					return b[s];
+				}
+			}else if(s == 1){
+				if((b[0] == b[1] && b[1] == b[2]) || (b[1] == b[4] && b[4] == b[7])){
+					return b[s];
+				}
+			}else if(s == 2){
+				if((b[0] == b[1] && b[1] == b[2]) || (b[2] == b[5] && b[5] == b[8]) || (b[2] == b[4] && b[4] == b[6])){
+					return b[s];
+				}
+			}else if(s == 3){
+				if((b[0] == b[3] && b[3] == b[6]) || (b[3] == b[4] && b[4] == b[5])){
+					return b[s];
+				}
+			}else if(s == 4){
+				if((b[3] == b[4] && b[4] == b[5]) || (b[1] == b[4] && b[4] == b[7]) || (b[0] == b[4] && b[4] == b[8] || (b[2] == b[4] && b[4] == b[6]))){
+					return b[s];
+				}
+			}else if(s == 5){
+				if((b[2] == b[5] && b[5] == b[8]) || (b[3] == b[4] && b[4] == b[5])){
+					return b[s];
+				}
+			}else if(s == 6){
+				if((b[6] == b[7] && b[7] == b[8]) || (b[0] == b[3] && b[3] == b[6]) || (b[2] == b[4] && b[4] == b[6])){
+					return b[s];
+				}
+			}else if(s == 7){
+				if((b[6] == b[7] && b[7] == b[8]) || (b[1] == b[4] && b[4] == b[7])){
+					return b[s];
+				}
+			}else if(s == 8){
+				if((b[6] == b[7] && b[7] == b[8]) || (b[2] == b[5] && b[5] == b[8]) || (b[0] == b[4] && b[4] == b[8])){
+					return b[s];
+				}
+			}
+		}
+		return 0;
+	}
 }
