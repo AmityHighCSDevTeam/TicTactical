27a4c618524ac267055db42f6eef6b35f8869e0a
SergeySave <sergeysav.nn@gmail.com>
2016-11-06 10:52:32 -0500

    adds an API for AIs to be added to the future singleplayer version

---
+++ core/src/org/amityregion5/tictactical/ai/IAI.java
@@ -0,0 +1,21 @@
+package org.amityregion5.tictactical.ai;
+
+/**
+ * An interface for an AI to play the game through
+ * 
+ * @author sergeys
+ *
+ */
+public interface IAI {
+	/**
+	 * Get the next move that this AI wants to make
+	 * 
+	 * This should always return a legal move
+	 * 
+	 * @param board the two dimensional array representing the entire board
+	 * @param bigboard the array representing who has won each small board
+	 * @param team the team that this AI is playing for
+	 * @return an array where the first element is the board the AI wants to make the move in and the second element is the tile within that board 
+	 */
+	public int[] chooseNextMove(char[][] board, char[] bigboard, char team);
+}
